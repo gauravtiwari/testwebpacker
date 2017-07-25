7690b0701093382dc9b413795a16371c73ea4011
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-07-25 08:55:56 +0100

    Add some more examples and restructure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,13 @@
 import Vue from 'vue/dist/vue.esm';
-import App from './app.vue';
+import App from '../app.vue';
 import Vuetify from 'vuetify';
 
 import 'vuetify/dist/vuetify.min.css'
+import '../app.sass'
 
 Vue.use(Vuetify);
 
 document.addEventListener('DOMContentLoaded', () => {
-
 	const app = new Vue({
 		el: '#app',
 		template: '<App/>',
@@ -15,5 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
 			App
 		}
 	});
-
 });
