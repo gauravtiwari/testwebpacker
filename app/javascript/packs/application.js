c15e05792d81c8e28bf3748c251f0bded430d59d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-07-25 08:49:31 +0100

    Upgrade webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import Vue from 'vue/dist/vue.esm';
 import App from './app.vue';
 import Vuetify from 'vuetify';
 
+import 'vuetify/dist/vuetify.min.css'
+
 Vue.use(Vuetify);
 
 document.addEventListener('DOMContentLoaded', () => {
